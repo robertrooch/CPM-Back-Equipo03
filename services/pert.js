6484e27b0a35ecf9cbde165ef4a39a4069146e1f
ETE12 <emilianotoes12@gmail.com>
2024-11-16 12:49:40 -0600

    Implementar el método nuevaActividad #5

--- services/pert.js
+++ services/pert.js
@@ -3,5 +3,27 @@ class Pert {
         this.idActividad = 0;
         this.Actividades = [];
     }
+    nuevaActividad(nombre, duracion, predecesores) {
+        this.idActividad += 1;
+        const actividad = {
+            id: this.idActividad,
+            nombre,
+            duracion: parseInt(duracion),
+            predecesores: predecesores.split(',').map(e => e.trim()),
+            sucesores: [],
+            estart: 0,
+            efinish: 0,
+            lstart: 0,
+            lfinish: 0,
+            hlibre: -1,
+            htotal: 0,
+            nivel: 0
+        };
+        if (actividad.predecesores.includes(actividad.nombre)) {
+            throw new Error(`La actividad ${actividad.nombre} no puede ser predecesora de sí misma`);
+        }
+        this.actividades.push(actividad);
+    }
+
 }
 module.exports = Pert;

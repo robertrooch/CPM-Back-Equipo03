2fc6598ef94566f43ec44bbfa7e57ea5aa9d22c5
robertrooch <robertorooch2004@gmail.com>
2024-11-20 00:03:52 -0600

    Implementar metodo earlyFinish y lateFinish #6 #7

--- services/pert.js
+++ services/pert.js
@@ -29,5 +29,37 @@ class Pert {
         this.actividades = this.actividades.filter(act => act.id !== id);
     }
 
+    earlyFinish(actividad) {
+        if (!actividad.predecesores[0]) {
+            actividad.nivel = 1;
+            actividad.efinish = actividad.duracion;
+        } else {
+            actividad.predecesores.forEach(predecesorNombre => {
+                const predecesor = this.locateRect(predecesorNombre);
+                predecesor.sucesores.push(actividad.nombre);
+                if (!predecesor.efinish) this.earlyFinish(predecesor);
+                actividad.estart = Math.max(actividad.estart, predecesor.efinish);
+                actividad.nivel = Math.max(actividad.nivel, predecesor.nivel + 1);
+            });
+            actividad.efinish = actividad.estart + actividad.duracion;
+        }
+    }
+
+    lateFinish(actividad, tiempoFinal) {
+        if (!actividad.sucesores.length) {
+            actividad.lfinish = tiempoFinal;
+            actividad.hlibre = tiempoFinal - actividad.efinish;
+        } else {
+            actividad.sucesores.forEach(sucesorNombre => {
+                const sucesor = this.locateRect(sucesorNombre);
+                if (!sucesor.lfinish) this.lateFinish(sucesor, tiempoFinal);
+                actividad.lfinish = actividad.lfinish > 0 ? Math.min(actividad.lfinish, sucesor.lstart) : sucesor.lstart;
+                actividad.hlibre = actividad.hlibre > -1 ? Math.min(actividad.hlibre, sucesor.estart - actividad.efinish) : sucesor.estart - actividad.efinish;
+            });
+        }
+        actividad.lstart = actividad.lfinish - actividad.duracion;
+        actividad.htotal = actividad.lfinish - actividad.efinish;
+    }
+
 }
 module.exports = Pert;
